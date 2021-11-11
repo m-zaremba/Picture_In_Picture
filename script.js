ceb10ae1b64795a99369e06b2beaeeeddb683cf3
m-zaremba <zaremba_michal@wp.pl>
2021-11-11 17:13:59 +0100

    picture in picture functionality added

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-window.addEventListener('DOMContentLoaded', (event) => { //Event necessary for script.js to work properly if <script> is placed in <meta>
-console.log('hello');
-  
-})
+//Event necessary for script.js to work properly if <script> is placed in <meta>
+window.addEventListener("DOMContentLoaded", (event) => {
+  const videoElement = document.getElementById("video");
+  const button = document.getElementById("button");
+
+  //Prompt for media source => pass it to video element => play
+  const selectMediaStream = async () => {
+    try {
+      const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+      videoElement.srcObject = mediaStream;
+      videoElement.onloadedmetadata = () => {
+        videoElement.play();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  button.addEventListener('click', async() => {
+    button.disabled = true;
+    await videoElement.requestPictureInPicture(); //Start picture in picture
+    button.disabled = false;
+  })
+
+  selectMediaStream();
+});
